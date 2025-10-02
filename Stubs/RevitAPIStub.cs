91a83b0d2056110080052c2782393269bb0be288
sherreratecnicos-cloud <sherrera.tecnicos@gmail.com>
2025-10-02 20:07:32 +0200

    Update RevitAPIStub.cs

--- Stubs/RevitAPIStub.cs
+++ Stubs/RevitAPIStub.cs
@@ -8,3 +8,28 @@ namespace Autodesk.Revit.DB
         public XYZ(double x, double y, double z) {}
     }
 }
+
+// Stubs/RevitAPIStub.cs
+namespace Autodesk.Revit.UI {
+    public interface IExternalCommand {
+        Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements);
+    }
+    public enum Result { Succeeded, Failed, Cancelled }
+    public class ExternalCommandData {}
+    public class ElementSet {}
+    public class UIControlledApplication {}
+    public interface IExternalApplication {
+        Result OnStartup(UIControlledApplication app);
+        Result OnShutdown(UIControlledApplication app);
+    }
+}
+
+// Stubs/RevitAttributes.cs
+namespace Autodesk.Revit.Attributes {
+    [System.AttributeUsage(System.AttributeTargets.Class)]
+    public class TransactionAttribute : System.Attribute {
+        public TransactionAttribute(TransactionMode mode) {}
+    }
+    public enum TransactionMode { Manual, Automatic }
+}
+
